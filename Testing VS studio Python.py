5d0ca7dab0fc2141353814248dff09b2e62f6787
Alpha_Omega <ganeshmba001@gmail.com>
2025-10-28 10:55:21 +0100

    this is a testing file from VS studio

---
+++ Testing VS studio Python.py
@@ -0,0 +1,11 @@
+print("Hello, World!"  )
+a=3
+b=4
+c=a+b
+print("Sum:",c)
+import pandas as pd
+import numpy as np
+!pip install scikit-learn
+from sklearn.model_selection import train_test_split
+df=pd.DataFrame({'A':[1,2,3],'B':[4,5,6]})
+print(df)
